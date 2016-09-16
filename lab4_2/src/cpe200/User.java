8fe8c3d47c440b8b3fd84b2627f9193ff37b3ebf
ISNE-1035 <poramin_s@cmu.ac.th>
2016-09-16 14:30:35 +0700

    Update User.java

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -1,28 +1,72 @@
 package cpe200;
 
+import java.util.regex.*;
+
 /**
  * Created by pruet on 6/9/2559.
  */
 public class User {
-    public String userName;
-    public String password;
+    protected String userName;
+    protected String password;
 
     public User() {
     }
 
     public boolean setUserName(String name) {
-        return false;
+        int a = 0;
+        if (name.length() < 8) {
+            return false;
+        }
+        if (name.length() >= 8) {
+            a++;
+        }
+        if (name.matches("(^[a-z]+)([a-zA-Z0-9]*)")) {
+            a++;
+        }
+        if (name.matches("(^[A-Z]+)([a-zA-Z0-9]*)")) {
+            a++;
+        }
+
+        if (a == 2) {
+            userName = name;
+            return true;
+
+        } else {
+            return false;
+        }
     }
 
     public boolean setPassword(String name) {
-        return false;
+        int a = 0;
+        if (name.length() < 12) {
+            return false;
+        }
+        if (name.length() >= 12) {
+            a++;
+        }
+        if (name.matches("(.*)([0-9]+)(.*)")) {
+            a++;
+        }
+        if (name.matches("(.*)([a-z]+)(.*)")) {
+            a++;
+        }
+        if (name.matches("(.*)([A-Z]+)(.*)")) {
+            a++;
+        }
+        if (a == 4) {
+            password = name;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public String getUserName() {
-        return null;
+
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+        return password;
     }
 }
